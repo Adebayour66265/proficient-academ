20690bb7aef8f39acd7cfa4a8fc3d379a2afe226
Mustapha Nurudeen <83666030+Adebayour66265@users.noreply.github.com>
2022-11-25 14:14:25 +0100

    Update database.js

--- data/database.js
+++ data/database.js
@@ -3,16 +3,10 @@ const mongodb = require('mongodb');
 
 const MongoClient = mongodb.MongoClient;
 
-
-let mongodbUrl = 'mongodb://localhost:27017';
-
-if (process.env.MONGODB_URL) {
-    mongodbUrl = process.env.MONGODB_URL;
-}
 let database;
 
 async function connectToDatabase() {
-    const client = await MongoClient.connect(mongodbUrl);
+    const client = await MongoClient.connect(process.env.MONGODB_URL);
     database = client.db('blog');
 }
 
